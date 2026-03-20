3e38d8ea13b53b2b81d907e543f6374bb959bf27
Sunbertrs <3127723014@qq.com>
2026-03-21 00:10:06 +0800

    feat: Add the converter files

---
+++ json2csv.py
@@ -0,0 +1,31 @@
+import json
+import csv
+import sys
+import os
+
+LOCAL_PATH = os.path.abspath(os.path.dirname(__file__))
+INPUT_FILE = os.path.abspath(sys.argv[1])
+OUTPUT_FILE = sys.argv[2] if len(sys.argv) > 2 else os.path.join(LOCAL_PATH, 'output.csv')
+COLUMN = ["关键词/表达式", "类型"]
+
+def process_input_file(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    processed_data = []
+    for block_type in (0,1):
+        for item in data:
+            if item["type"] == block_type:
+                processed_data.append(item)
+    return processed_data
+
+def write_csv(data, file):
+    with open(file, 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(COLUMN)
+        for item in data:
+            writer.writerow([item["filter"], item["type"]])
+
+if __name__ == "__main__":
+    data = process_input_file(INPUT_FILE)
+    write_csv(data, OUTPUT_FILE)
+
